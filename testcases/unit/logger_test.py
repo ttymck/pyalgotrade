db39c9f492e7dfdbaa9e927b2155ddf71e554787
Tyler M. Kontra <46075030+ttymck@users.noreply.github.com>
2019-12-27 01:19:54 -0500

    feat(build): poetry init (#1)
    * feat(build): add poetry
    
    * feat(pyproject.toml): pull in missing info
    
    * feat(build): include package in pyproject.toml
    
    * chore(build): remove setup.py
    
    * chore(tests): separate tests into unit, integration
    - clean up logger tests; uses captured stdout
    - remove doc_test
    
    * fix(testcases/unit): fixes unittest method deprecation warnings
    
    * fix(testcases/integration): skip failing tests, fix deprecation warnings
    
    * chore(build): no docker travis
    - clean up unnecessary scripts
    
    * fix(travis): poetry install
    
    * fix(travis): install ta-lib
    
    * fix(travis): encrypt new quandl key
    
    * fix(travis): secure env

---
+++ testcases/unit/logger_test.py
@@ -0,0 +1,69 @@
+# PyAlgoTrade
+#
+# Copyright 2011-2018 Gabriel Martin Becedillas Ruiz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+.. moduleauthor:: Gabriel Martin Becedillas Ruiz <gabriel.becedillas@gmail.com>
+"""
+
+from contextlib import redirect_stderr
+import datetime, io
+
+from testcases import common
+
+from pyalgotrade import strategy
+from pyalgotrade import bar
+from pyalgotrade import logger
+from pyalgotrade.barfeed import membf
+
+
+class MockBarFeed(membf.BarFeed):
+    def barsHaveAdjClose(self):
+        raise NotImplementedError()
+
+
+class MockStrategy(strategy.BacktestingStrategy):
+    def __init__(self, barFeed, cash):
+        logger.rootLoggerInitialized = False
+        strategy.BacktestingStrategy.__init__(self, barFeed, cash)
+
+    def onBars(self, bars):
+        self.info("bla")
+        logger.getLogger("custom").info("ble")
+
+    @classmethod
+    def run_test(cls):
+        bf = MockBarFeed(bar.Frequency.DAY)
+        bars = [
+            bar.BasicBar(datetime.datetime(2000, 1, 1), 10, 10, 10, 10, 10, 10, bar.Frequency.DAY),
+        ]
+        bf.addBarsFromSequence("orcl", bars)
+
+        strat = MockStrategy(bf, 1000)
+        strat.run()
+
+
+class TestCase(common.TestCase):
+    # Check that strategy and custom logs have the proper datetime, this is, the bars date time.
+    def testBacktestingLog(self):
+        stdout = io.StringIO()
+        with redirect_stderr(stdout):
+            MockStrategy.run_test()
+
+        expectedLines = [
+            "2000-01-01 00:00:00 strategy [INFO] bla",
+            "2000-01-01 00:00:00 custom [INFO] ble",
+        ]
+        self.assertEqual([line.strip() for line in stdout.getvalue().split("\n") if line.strip() != ""], expectedLines)
